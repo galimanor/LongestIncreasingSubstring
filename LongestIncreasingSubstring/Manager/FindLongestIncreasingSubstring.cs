4cf8729616758e70133ecfda386fcb6604fa0960
galimanor <gali.manor@gmail.com>
2024-02-19 14:11:07 +1100

    Add logic

---
+++ LongestIncreasingSubstring/Manager/FindLongestIncreasingSubstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestIncreasingSubstring.Manager
+{
+    public class FindLongestSubstringManager
+    {
+
+        public List<int> GetLongestSubstring(string input)
+        {
+            var numbersStr = input.Split(' ');
+            var numbers = new List<int>();
+            var longestSubstring = new List<int>();
+
+
+            foreach (var numStr in numbersStr)
+            {
+                if (int.TryParse(numStr, out int num))
+                {
+                    numbers.Add(num);
+                }
+            }
+
+            if (numbers.Any())
+            {
+                var currentSubstring = new List<int> { numbers[0] };
+                for (var i = 0; i < numbers.Count - 1; i++)
+                {
+                    if (numbers[i] < numbers[i + 1])
+                        currentSubstring.Add(numbers[i + 1]);
+                    else if (currentSubstring.Count > longestSubstring.Count)
+                    {
+                        longestSubstring = currentSubstring;
+                        currentSubstring = new List<int> { numbers[i + 1] };
+                    }
+                    else
+                    {
+                        currentSubstring = new List<int> { numbers[i + 1] };
+                    }
+                }
+
+                return longestSubstring.Any() ? longestSubstring : currentSubstring;
+            }
+
+            return new List<int>();
+        }
+    }
+}
---
+++ LongestIncreasingSubstring/Program.cs
@@ -0,0 +1,37 @@
+﻿using LongestIncreasingSubstring.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LongestIncreasingSubstring
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter your list of numbers: ");
+            var userInput = Console.ReadLine();
+
+            while (userInput == null || userInput == string.Empty || Regex.Replace(userInput, @"\s+", "") == string.Empty)
+            {
+                Console.WriteLine("Please enter a valid list of numbers: ");
+                userInput = Console.ReadLine();
+            }
+
+            var findLongestSubstringManager = new FindLongestSubstringManager();
+            var longestIncreasingSubsequence = findLongestSubstringManager.GetLongestSubstring(userInput);
+
+            // Print the result
+            if (!longestIncreasingSubsequence.Any())
+                Console.WriteLine("Your input didn't include any valid numbers.");
+            else
+            {
+                Console.WriteLine("Longest increasing subsequence:");
+                Console.WriteLine(string.Join(" ", longestIncreasingSubsequence));
+            }
+        }
+    }
+}
